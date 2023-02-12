1c9c52e35295306a1b7d34f660aa7a2519409b88
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-02-12 23:19:13 +0000

    added vercel instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+// import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://gh-issues-astro-microblog.netlify.app",
   integrations: [sitemap(), tailwind()],
   output: "server",
-  adapter: netlify(),
-  vite: {
-    ssr: {
-      external: ["sanitize-html", "@octokit/request"],
-    },
-  },
+  adapter: vercel(),
+  // vite: {
+  //   ssr: {
+  //     external: ["sanitize-html", "@octokit/request"]
+  //   }
+  // }
 });
