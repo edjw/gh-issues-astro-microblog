79faeb9e3ce4f1fcb0343b1f18b298d4e6237c67
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-02-13 08:38:51 +0000

    allow images

--- src/utils/parseMD.ts
+++ src/utils/parseMD.ts
@@ -1,6 +1,10 @@
 import { marked } from "marked";
 import sanitizeHtml from "sanitize-html";
 
+const options = {
+  allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+};
+
 export const parseMD = (content: string) => {
-  return sanitizeHtml(marked.parse(content));
+  return sanitizeHtml(marked.parse(content), options);
 };
