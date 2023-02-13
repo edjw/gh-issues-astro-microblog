0cce7eeb4c056a2126f01bfdc9e018803b6b7851
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-02-13 12:10:25 +0000

    tags working

--- src/data/getGithubIssues.ts
+++ src/data/getGithubIssues.ts
@@ -5,6 +5,7 @@ import type { Endpoints } from "@octokit/types";
 
 type GithubIssue =
   Endpoints["GET /repos/{owner}/{repo}/issues"]["response"]["data"][0];
+
 export type GithubIssueWithSlug = GithubIssue & { slug: string };
 
 import { repoOwner, repoName, postsState, showTitles } from "../consts";
