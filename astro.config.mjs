ecf5f651f0f58f2128f65effe0185c5fb1f6d2d0
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-02-14 22:20:10 +0000

    remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   site: "https://gh-issues-astro-microblog.netlify.app",
-  integrations: [sitemap(), tailwind()],
+  integrations: [tailwind()],
   output: "server",
   adapter: vercel(),
 });
