a20faf90dc25b50bfc6b7442c7a7198b8f520943
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-02-17 11:48:10 +0000

    auto convert video links to a video tag

--- src/utils/parseMD.ts
+++ src/utils/parseMD.ts
@@ -1,15 +1,41 @@
 import { marked } from "marked";
 import sanitizeHtml from "sanitize-html";
 
-const options = {
-  allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img", "video", "source"]),
-  allowedAttributes: {
-  video: [ 'src'],
-source: [ 'src', 'type' ],
-},
+const videoFileTypes = [".mov", ".mp4", ".webm", ".ogg"];
+const isURLVideo = (url: string): Boolean => {
+  const fileType = url.substring(url.lastIndexOf(".")).toLowerCase();
+  return videoFileTypes.includes(fileType);
+};
 
+const renderer = new marked.Renderer();
+renderer.link = (href, title, text): string => {
+  if (href !== null && isURLVideo(href)) {
+    return `<video controls>
+              <source src="${href}" type="video/mp4">
+            </video>`;
+  } else if (title !== null && title !== undefined) {
+    return `<a href="${href}" title="${title}">${text}</a>`;
+  } else {
+    return `<a href="${href}">${text}</a>`;
+  }
+};
+
+marked.use({ renderer });
+
+const sanitizeHTMLOptions: sanitizeHtml.IOptions = {
+  allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+    "img",
+    "video",
+    "source",
+  ]),
+  allowedAttributes: {
+    video: ["src", "controls", "type", "muted", "autoplay"],
+    source: ["src", "type"],
+    img: ["src", "alt"],
+    a: ["href", "title"],
+  },
 };
 
 export const parseMD = (content: string) => {
-  return sanitizeHtml(marked.parse(content), options);
+  return sanitizeHtml(marked.parse(content), sanitizeHTMLOptions);
 };
