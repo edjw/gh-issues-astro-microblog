d14fab27d08a8978138690bd47601400e3b25696
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-08-22 22:15:24 +0100

    video works; won't overwrite images and videos in cloudinary any more

--- src/utils/parseMD.ts
+++ src/utils/parseMD.ts
@@ -1,8 +1,6 @@
 import { marked } from "marked";
 import sanitizeHtml from "sanitize-html";
 import { v2 as cloudinary } from "cloudinary";
-// import fetch from "node-fetch";
-// import { https } from "follow-redirects";
 import pkg from "follow-redirects";
 const { https } = pkg;
 import { ASSETS_URL } from "../consts";
@@ -12,25 +10,26 @@ const CLOUDINARY_CLOUD_NAME = import.meta.env.CLOUDINARY_CLOUD_NAME;
 const CLOUDINARY_API_KEY = import.meta.env.CLOUDINARY_API_KEY;
 const CLOUDINARY_API_SECRET = import.meta.env.CLOUDINARY_API_SECRET;
 
-const videoFileTypes = [".mov", ".mp4"];
-
+// Checks if a link is for a video
 const isURLVideo = (url: string): boolean => {
   if (!url) return false;
-  const fileType = url.substring(url.lastIndexOf(".")).toLowerCase();
+  const videoFileTypes = [".mov", ".mp4"];
+  const base = new URL(url).pathname;
+  const fileType = base.substring(base.lastIndexOf(".")).toLowerCase();
   const isVideo = videoFileTypes.includes(fileType);
-  // console.log(`URL: ${url}, isVideo: ${isVideo}`);
   return isVideo;
 };
 
+// Checks if a url is from the ASSETS_URL
 const isAssetURL = (url: string): boolean => {
   return url.startsWith(ASSETS_URL);
 };
 
+// Gets the Amazon S3 url for new Github assets urls
 const fetchRedirectUrl = async (url: string): Promise<string | null> => {
   if (!url.startsWith(ASSETS_URL)) {
-    return url; // Return the original URL if it doesn't start with ASSETS_URL
+    return url;
   }
-  // console.log(`Fetching redirect URL for: ${url}`);
 
   return new Promise((resolve, reject) => {
     https
@@ -49,17 +48,16 @@ const fetchRedirectUrl = async (url: string): Promise<string | null> => {
           const { statusCode, statusMessage, responseUrl } = response;
 
           if (statusCode && statusCode >= 200 && statusCode < 400) {
-            // console.log(`Final URL: ${responseUrl}`);
             resolve(responseUrl);
           } else {
             console.error(
               `Error fetching URL: ${url}. Status: ${statusCode} - ${statusMessage}`
             );
-            // console.log({ response });
-            resolve(null); // Use resolve with null instead of reject to mimic the previous behavior
+
+            resolve(null);
           }
 
-          response.on("data", () => {}); // Consume response data to free up memory
+          response.on("data", () => {});
         }
       )
       .on("error", (err) => {
@@ -76,17 +74,17 @@ cloudinary.config({
   api_secret: CLOUDINARY_API_SECRET,
 });
 
+// Uploads images and videos to cloudinary during the build
 const uploadToCloudinary = async (url: string, type: "image" | "video") => {
   const options = {
     use_filename: true,
     unique_filename: false,
-    overwrite: true,
+    overwrite: false,
     resource_type: type,
   };
 
   try {
     const result = await cloudinary.uploader.upload(url, options);
-    // console.log("Uploaded to Cloudindary: ", result.secure_url, "\n");
     return result;
   } catch (error) {
     console.error("Error uploading to Cloudinary:", { error, url, type });
@@ -94,6 +92,7 @@ const uploadToCloudinary = async (url: string, type: "image" | "video") => {
   }
 };
 
+// Finds urls in the markdown content
 const extractUrlsFromMarkdown = (content: string): string[] => {
   const urls: string[] = [];
   const tempRenderer = new marked.Renderer();
@@ -110,11 +109,7 @@ const extractUrlsFromMarkdown = (content: string): string[] => {
   };
 
   tempRenderer.image = (href) => {
-    if (
-      isURLVideo(href) ||
-      href.match(/\.(jpeg|jpg|gif|png)$/i) ||
-      isAssetURL(href)
-    ) {
+    if (href.match(/\.(jpeg|jpg|gif|png)$/i) || isAssetURL(href)) {
       urls.push(href);
     }
     return href;
@@ -124,6 +119,8 @@ const extractUrlsFromMarkdown = (content: string): string[] => {
   marked.parse(content);
   return urls;
 };
+
+// Loops through all urls, works out if they're an image or a video. Uploads images and videos to Cloudinary.
 const processUrls = async (urls: string[]): Promise<Map<string, string>> => {
   const urlMap: Map<string, string> = new Map();
 
@@ -133,13 +130,8 @@ const processUrls = async (urls: string[]): Promise<Map<string, string>> => {
 
     if (!finalUrl) {
       console.error("Final URL not found for: ", url);
-      continue; // Skip to the next iteration without trying to upload
+      continue;
     }
-
-    // Change the asset type determination to be based on the finalUrl, not the original URL
-    // console.log(`Asset Type for URL ${finalUrl} is ${assetType}`);
-
-    // console.log(`Final URL: ${finalUrl}, assetType: ${assetType}`); // Debugging line
 
     const transformedHref = await uploadToCloudinary(finalUrl, assetType);
 
@@ -187,6 +179,11 @@ export const parseMD = async (content: string): Promise<string> => {
       isAssetURL(href)
     ) {
       const processedUrl = processedUrlMap.get(href) || href;
+      if (isURLVideo(processedUrl)) {
+        return `<div class="flex justify-center"><video controls muted preload="metadata" class="max-h-72 my-0 border dark:border-gray-500">
+              <source src="${processedUrl}" type="video/mp4">
+            </video></div>`;
+      }
       return `<a href="${processedUrl}">${text}</a>`;
     }
     return `<a href="${href}">${text}</a>`;
