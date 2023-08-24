277bce7c00705d4f2d72e1aec07cab5cdbb18914
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-08-24 09:38:12 +0100

    revert redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,19 +14,6 @@ export default defineConfig({
   experimental: {
     viewTransitions: true
   },
-  redirects: {
-    '/': '/posts/1',
-    '/24-little-lego-castle': '/post/24-little-lego-castle',
-    '/23-the-needles-on-the-isle-of-wight': '/post/23-the-needles-on-the-isle-of-wight',
-    '/22-next-book-foghorns': '/post/22-next-book-foghorns',
-    '/21-posting-lastfm-summaries-and-this-cms': '/post/21-posting-lastfm-summaries-and-this-cms',
-    '/20-my-lastfm-weekly-tracks-chart': '/post/20-my-lastfm-weekly-tracks-chart',
-    '/12-holy-fucks-music': '/post/12-holy-fucks-music',
-    '/5-so-quick': '/post/5-so-quick',
-    '/3-websites-are-nicer-to-publish-than-native-apps': '/post/3-websites-are-nicer-to-publish-than-native-apps',
-    '/2-lighthouse': '/post/2-lighthouse',
-    '/1-first-post': '/post/1-first-post'
-  }
 
   // output: "static",
   // adapter: vercel({
