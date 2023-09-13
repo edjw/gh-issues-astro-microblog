9102737d5a76453efd7101d9304279dac1c21f7f
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-09-13 16:04:40 +0100

    undo vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://microblog.edjohnsonwilliams.co.uk",
   integrations: [tailwind(), svelte()],
   output: "static",
-  adapter: vercel()
+
   // adapter: vercel({
   //   includeFiles: [
   //     "./public/fonts/Raleway-Regular.ttf",
